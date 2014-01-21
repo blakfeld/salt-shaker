d75bdf691d66f5344f537f3b624b1d64c436a07e
Corwin Brown <Corwin.Brown@untsystem.edu>
2014-01-21 06:57:49 -0600

    Prettied up my dynamic alerts Also cleaned up file structure a bit, and slimmed down the verison of bootstrap i was using

--- public/javascripts/bootstrap_alerts.js
+++ public/javascripts/bootstrap_alerts.js
@@ -1,11 +1,12 @@
 bootstrap_alert = function(type, message, timeout) {
   $('.alert').alert('close');
-  $('#alerts').prepend('<div class="alert alert-'+ type + '"><a class="close" style="margin-bottom: 4px" data-dismiss="alert">&times;</a>' + message + '<div>');
-
+  $('.dynamic_alerts').append('<div class="alert alert-'+ type + '"><a class="close" style="margin-bottom: 4px" data-dismiss="alert">&times;</a>' + message + '<div>').hide().slideDown('slow', 'swing');
 
   if (timeout || timeout === 0) {
     setTimeout(function() {
-      $(".alert").alert('close');
-    }, timeout);    
+      $('.dynamic_alerts').slideUp('slow', 'swing', function(){
+        $(".alert").alert('close');
+      });
+    }, timeout);
   }
 };
